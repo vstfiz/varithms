f823136e5a21eac3aff74dfdd6e77cf05d09b416
vstfiz <vivek.sharma7354@gmail.com>
2020-08-08 14:18:44 +0530

    added fire_auth.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,8 @@
+import 'package:Varithms/dashboard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -24,4 +27,35 @@ class _LoginState extends State<Login> {
       ),
     );
   }
+
+  Widget _googleSignIn(BuildContext context1) {
+    return OutlineButton(
+        splashColor: Colors.grey,
+        onPressed: ()
+    {
+      signInWithGoogle().whenComplete(() {
+        if (checkLogIn() == true) {
+          ifUserExists().whenComplete(() {
+            if (userExists) {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return DashBoard();
+                  },
+                ),
+              );
+            }
+            else {
+              nameController.text = name;
+              emailController.text = email;
+              userdp = imageUrl;
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return FillDetails();
+                  },
+                ),
+              );
+            }
+          });
 }
