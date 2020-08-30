cfeb0487dc073bd40e43f915506ca47ff03781b9
vstfiz <vivek.sharma7354@gmail.com>
2020-08-30 20:53:42 +0530

    added play button dart

---
+++ lib/play_button.dart
@@ -0,0 +1,154 @@
+import 'dart:math';
+
+import 'package:Varithms/globals.dart' as globals;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PlayButton extends StatefulWidget {
+  final bool initialIsPlaying;
+  final Icon playIcon;
+  final Icon pauseIcon;
+  final VoidCallback onPressed;
+
+  PlayButton({
+    @required this.onPressed,
+    this.initialIsPlaying = false,
+    this.playIcon = const Icon(Icons.play_arrow),
+    this.pauseIcon = const Icon(Icons.pause),
+  }) : assert(onPressed != null);
+
+  @override
+  _PlayButtonState createState() => _PlayButtonState();
+}
+
+class _PlayButtonState extends State<PlayButton> with TickerProviderStateMixin {
+  static const _kToggleDuration = Duration(milliseconds: 300);
+  static const _kRotationDuration = Duration(seconds: 5);
+
+  bool isPlaying;
+
+  // rotation and scale animations
+  AnimationController _rotationController;
+  AnimationController _scaleController;
+  double _rotation = 0;
+  double _scale = 0.85;
+
+  bool get _showWaves => !_scaleController.isDismissed;
+
+  void _updateRotation() => _rotation = _rotationController.value * 2 * pi;
+
+  void _updateScale() => _scale = (_scaleController.value * 0.2) + 0.85;
+
+  @override
+  void initState() {
+    _rotationController =
+        AnimationController(vsync: this, duration: _kRotationDuration)
+          ..addListener(() => setState(_updateRotation))
+          ..repeat();
+
+    _scaleController =
+        AnimationController(vsync: this, duration: _kToggleDuration)
+          ..addListener(() => setState(_updateScale));
+
+    super.initState();
+  }
+
+  void _onToggle() {
+    if (_scaleController.isCompleted) {
+      _scaleController.reverse();
+    } else {
+      _scaleController.forward();
+    }
+    widget.onPressed();
+  }
+
+  Widget _buildIcon() {
+    return SizedBox.expand(
+      child: IconButton(
+        icon: globals.isPlaying ? widget.pauseIcon : widget.playIcon,
+        onPressed: _onToggle,
+        iconSize: 48,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(minWidth: 48, minHeight: 48),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          if (_showWaves) ...[
+            globals.isPlaying
+                ? Blob(
+                    color: Color(0xff0092ff),
+                    scale: _scale,
+                    rotation: _rotation)
+                : SizedBox(),
+            globals.isPlaying
+                ? Blob(
+                    color: Color(0xff4ac7b7),
+                    scale: _scale,
+                    rotation: _rotation * 2 - 30)
+                : SizedBox(),
+            globals.isPlaying
+                ? Blob(
+                    color: Color(0xffa4a6f6),
+                    scale: _scale,
+                    rotation: _rotation * 3 - 45)
+                : SizedBox(),
+          ],
+          Container(
+            constraints: BoxConstraints.expand(),
+            child: AnimatedSwitcher(
+              child: _buildIcon(),
+              duration: _kToggleDuration,
+            ),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.grey),
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scaleController.dispose();
+    _rotationController.dispose();
+    super.dispose();
+  }
+}
+
+class Blob extends StatelessWidget {
+  final double rotation;
+  final double scale;
+  final Color color;
+
+  const Blob({this.color, this.rotation = 0, this.scale = 1});
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.scale(
+      scale: scale,
+      child: Transform.rotate(
+        angle: rotation,
+        child: Container(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(150),
+              topRight: Radius.circular(240),
+              bottomLeft: Radius.circular(220),
+              bottomRight: Radius.circular(180),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
